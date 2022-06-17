0b4791d50ef6b9e3189d829b4781c6a622992517
liuqixing <liuqixing@dekuan.org>
2022-06-17 19:49:58 +0800

    Added new method RestErrors.isSuccess( ... );

--- src/main/java/org/dekuan/airpassport/lib/constants/RestErrors.java
+++ src/main/java/org/dekuan/airpassport/lib/constants/RestErrors.java
@@ -6,4 +6,14 @@ public class RestErrors
 	public final static String UNKNOWN		= "unknown";
 	public final static String FAILED		= "failed";
 	public final static String INVALID_PAYLOAD	= "invalid_payload";
+
+	public static boolean isSuccess( String error )
+	{
+		if ( null == error )
+		{
+			return false;
+		}
+
+		return SUCCESS.equalsIgnoreCase( error.trim() );
+	}
 }
