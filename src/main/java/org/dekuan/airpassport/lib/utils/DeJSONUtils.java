038e7abcdfe0ee5c596c7c2138a58204695228f2
liuqixing <liuqixing@dekuan.org>
2022-07-10 22:09:03 +0800

    Added DeJSONUtils

---
+++ src/main/java/org/dekuan/airpassport/lib/utils/DeJSONUtils.java
@@ -0,0 +1,78 @@
+package org.dekuan.airpassport.lib.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
+import org.json.JSONObject;
+
+@Slf4j
+public class DeJSONUtils
+{
+	public double parseDouble( JSONObject jsonObject, String keyName )
+	{
+		return parseDouble( jsonObject, keyName, Double.NaN );
+	}
+	public double parseDouble( JSONObject jsonObject, String keyName, double defaultValue )
+	{
+		if ( null == jsonObject )
+		{
+			return defaultValue;
+		}
+		if ( Strings.isBlank( keyName ) )
+		{
+			return defaultValue;
+		}
+
+		String stringValue = jsonObject.optString( keyName, "" );
+		if ( Strings.isBlank( stringValue ) )
+		{
+			return defaultValue;
+		}
+
+		try
+		{
+			return Double.parseDouble( stringValue );
+		}
+		catch ( NumberFormatException e )
+		{
+			e.printStackTrace();
+			log.error( e.getMessage() );
+		}
+
+		return defaultValue;
+	}
+
+
+	public int parseInt( JSONObject jsonObject, String keyName )
+	{
+		return parseInt( jsonObject, keyName, 0 );
+	}
+	public int parseInt( JSONObject jsonObject, String keyName, int defaultValue )
+	{
+		if ( null == jsonObject )
+		{
+			return defaultValue;
+		}
+		if ( Strings.isBlank( keyName ) )
+		{
+			return defaultValue;
+		}
+
+		String stringValue = jsonObject.optString( keyName, "" );
+		if ( Strings.isBlank( stringValue ) )
+		{
+			return defaultValue;
+		}
+
+		try
+		{
+			return Integer.parseInt( stringValue );
+		}
+		catch ( NumberFormatException e )
+		{
+			e.printStackTrace();
+			log.error( e.getMessage() );
+		}
+
+		return defaultValue;
+	}
+}
