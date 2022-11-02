20b2b87ca81a3bf8a72bc64439704562175019ba
liuqixing <liuqixing@dekuan.org>
2022-11-03 05:45:05 +0800

    Added PAUSED option to CommonStatus

--- src/main/java/org/dekuan/airpassport/lib/constants/CommonStatus.java
+++ src/main/java/org/dekuan/airpassport/lib/constants/CommonStatus.java
@@ -3,13 +3,19 @@ package org.dekuan.airpassport.lib.constants;
 public class CommonStatus
 {
 	public static final int UNSET		= 0;
-	public static final int OKAY		= 1;
-	public static final int ARCHIVED	= 2;
-	public static final int DELETED		= 3;
+	public static final int OKAY		= 1;	//	一切正常
+	public static final int ARCHIVED	= 2;	//	已归档的
+	public static final int DELETED		= 3;	//	已删除的
+	public static final int PAUSED		= 4;	//	已暂停的
 
 
 	public static boolean isValid( int status )
 	{
-		return UNSET == status || OKAY == status || ARCHIVED == status || DELETED == status;
+		return UNSET == status ||
+			OKAY == status ||
+			ARCHIVED == status ||
+			DELETED == status ||
+			PAUSED == status
+		;
 	}
 }
