000a31063623c6e47ac7eeccb118d72aff88ea4d
liuqixing <liuqixing@dekuan.org>
2022-11-22 22:56:20 +0800

    Upgraded DeValidationUtil.isValidUUID

--- src/main/java/org/dekuan/airpassport/lib/utils/DeValidationUtil.java
+++ src/main/java/org/dekuan/airpassport/lib/utils/DeValidationUtil.java
@@ -1,6 +1,5 @@
 package org.dekuan.airpassport.lib.utils;
 
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.validation.FieldError;
 
 import java.util.UUID;
@@ -13,28 +12,28 @@ public class DeValidationUtil
 		return null != fieldError ? String.format( "Field error in object '%s' on field '%s': rejected value [%s]; message: %s", fieldError.getObjectName(), fieldError.getField(), fieldError.getRejectedValue(), fieldError.getDefaultMessage() ) : "null field error";
 	}
 
-	public static boolean isValidUUID( String testUUID )
+	public static boolean isValidUUID( UUID testUUID )
 	{
-		if ( Strings.isBlank( testUUID ) )
-		{
-			return false;
-		}
-
-		boolean isValid = false;
-
 		try
 		{
-			UUID uuid = UUID.fromString( testUUID );
-			isValid = true;
-		}
-		catch ( IllegalArgumentException exception )
-		{
-			//	handle the case where string is not valid UUID
+			return 0 == UUID.fromString( testUUID.toString() ).compareTo( testUUID );
 		}
 		catch ( Exception ignored )
 		{
+			return false;
 		}
+	}
 
-		return isValid;
+	public static boolean isValidUUID( String testUUID )
+	{
+		try
+		{
+			return 0 == UUID.fromString( testUUID ).toString().compareToIgnoreCase( testUUID );
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
---
+++ src/test/java/org/dekuan/airpassport/lib/DeValidationUtilTests.java
@@ -0,0 +1,24 @@
+package org.dekuan.airpassport.lib;
+
+import org.dekuan.airpassport.lib.utils.DeValidationUtil;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DeValidationUtilTests
+{
+	@Test
+	public void testisValidUUIDCase1()
+	{
+		assertTrue( DeValidationUtil.isValidUUID( "8D1824CA-2B7F-4EC9-ADB4-4572195FA355" ) );
+	}
+
+	@Test
+	public void testisValidUUIDCase2()
+	{
+		UUID uuid = UUID.fromString( "8D1824CA-2B7F-4EC9-ADB4-4572195FA355" );
+		assertTrue( DeValidationUtil.isValidUUID( uuid ) );
+	}
+}
